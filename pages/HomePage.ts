995e2d66de693d191437dde7624c039224ff747e
gauravchauhan200 <gauravchauhan200@gmail.com>
2025-09-18 08:34:11 +0530

    FirstCommit

---
+++ pages/HomePage.ts
@@ -0,0 +1,65 @@
+import {Page,expect,Locator} from '@playwright/test';
+
+export class HomePage{
+
+private readonly page: Page;
+
+//locators
+private readonly lnkMyAccount:  Locator;
+private readonly lnkRegister:   Locator;
+private readonly linklogin:     Locator;
+private readonly txtSearchbox:  Locator;
+private readonly btnSearch:     Locator;
+
+
+//--------constructors---------
+
+constructor(page:Page)
+{
+    this.page=page;
+    this.lnkMyAccount=page.locator('span:has-text("My Account")');
+    this.lnkRegister=page.locator('a:has-text("Register")');
+    this.linklogin=page.locator('a:has-text("Login")');
+    this.txtSearchbox=page.locator('input[name="search"]');
+    this.btnSearch=page.locator('span[class="input-group-btn"]>button[type="button"]');
+}
+
+//---------action method--------
+
+        //check if HomePage exist
+async isHomePageExist()
+{
+    let title:string = await this.page.title();
+    if(title)
+    {
+        return true;
+    }
+    return false;
+
+}
+
+// Click "My Account" link
+
+async clickMyAccount()
+{   try{
+    await this.lnkMyAccount.click();
+    }catch(error){
+        console.log(`Exception occured while checking 'My Account':${error}`);
+        throw error;
+    }
+
+
+
+}
+
+
+
+
+
+
+}
+
+//action method
+
+
+}
